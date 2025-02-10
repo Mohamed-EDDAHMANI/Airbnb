dfacd174b55a44d23e6bb80c5ee3736c433bf125
Mohamed-EDDAHMANI <medd.eddahmani@gmail.com>
2025-02-10 10:40:19 +0100

    -> 10/02.10:40 -> Hey dudes, please pull the Update Code ^_-

--- project/App/config/routes.php
+++ project/App/config/routes.php
@@ -1,22 +1,43 @@
 <?php
 
-$router->get('listAnnonce','HomeController@getAllAnnonce');
-$router->get('home','HomeController@getTopAnnonce');
-$router->post('home','HomeController@getTopCommentaire');
+$router->get('/home','HomeController@gethomePage');
+// ---- on the get home page (Page with Auth)
+// $router->get('/home/listAnnonce','HomeController@getAllAnnonce');
+$router->get('/detailsAnnonce','HomeController@detailsAnnonce');
+$router->get('/reservationPage','HomeController@getReservationPage');
+$router->get('/reservationPage/reserve','HomeController@reserve');
+$router->get('/myReservations','HomeController@myReservations');
+$router->get('/myReservations/delete/{id}','HomeController@reservationsDelete');
+$router->get('/payementPage','HomeController@getpayementPage');
+$router->get('/payementPage/payer','HomeController@effectuerPayement');
 
+//sans auth
+$router->get('/index','HomeController@getIndexPage');
+// ---- on the get index page (Page without Auth)
+// $router->get('/index/listAnnonce','HomeController@listAnnonce');
+$router->get('/index/getAllAnnonce','HomeController@getAllAnnonce');
+$router->get('/index/getTopAnnonce','HomeController@getTopAnnonce');
+$router->post('/index/getTopCommentaire','HomeController@getTopCommentaire');
+
+$router->get('/admin', 'AdminController@adminDashboard');
 $router->get('/admin/users', 'AdminController@getAllUsers');
-$router->get('admin/delete/user', 'AdminController@getAllAnnonces');
-$router->get('admin', 'AdminController@getStatistiques');
-$router->get('admin', 'AdminController@getPopulairePropritaire');
-$router->get('admin', 'AdminController@getRevenux');
-$router->post('admin', 'AdminController@validationAnnonce');
-$router->post('admin', 'AdminController@validationUser');
-$router->post('admin', 'AdminController@deleteAnnonce');
-$router->post('admin', 'AdminController@deleteCommentaires');
-$router->post('admin', 'AdminController@gestionLitige');
+$router->get('/admin/getAnnonces', 'AdminController@getAllAnnonces');
+$router->get('/admin/statistiques', controller: 'AdminController@getStatistiques');
+$router->get('/admin/getPopulairePropritaire', 'AdminController@getPopulairePropritaire');
+$router->get('/admin/getRevenux', 'AdminController@getRevenux');
+$router->post('/admin/validationAnnonce', 'AdminController@validationAnnonce');
+$router->post('/admin/validationUser', 'AdminController@validationUser');
+$router->post('/admin/delete/{id}', 'AdminController@deleteAnnonce');
+$router->post('/admin/deleteCommentaires', 'AdminController@deleteCommentaires');
+$router->post('/admin/gestionLitige', 'AdminController@gestionLitige');
 
-$router->get(route: 'proprietaire', controller: 'proprietaireController@');
-$router->post('proprietaire', 'proprietaireController');
+$router->get(route: '/proprietaire', controller: 'proprietaireController@proprietaireDashboard');
+$router->get(route: '/myAnnonces', controller: 'proprietaireController@getMyAnnonces');
+$router->get(route: '/getAnnonceByid/{id}', controller: 'proprietaireController@getAnnonceByid');
+$router->get(route: '/getReservations', controller: 'proprietaireController@getReservations');
+$router->post('/createAnnonce', 'proprietaireController@createAnnonce');
+$router->post('/deleteAnnonce/{id}', 'proprietaireController@deleteAnnonce');
+$router->post('/UpdateAnnonce/{id}', 'proprietaireController@UpdateAnnonce');
 
 $router->get('login', 'AuthController@getLoginPage');
 $router->post('login', 'AuthController@postLoginPage');
--- project/App/controllers/AuthController.php
+++ project/App/controllers/AuthController.php
@@ -1,10 +1,11 @@
 <?php
 namespace App\Controllers;
-
 use App\Core\Controller;
+use App\Models\UserModel;
 use App\Classes\User;
 
 class AuthController extends Controller {
+
     private $userModel;
 
     public function __construct(){
@@ -15,6 +16,7 @@ class AuthController extends Controller {
         // require  __DIR__ . '/../Views/auth/login.php';
         $this->view('auth/login', );
     }
+    
     public function postLoginPage(): void {
         $user = new User($_POST['email'], $_POST['password']);
     }
