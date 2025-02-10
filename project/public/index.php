1588f5bcadc09abcb78003c95478e0231851292d
mohammedennaim <mohammedennaim99@gmail.com>
2025-02-10 14:38:24 +0100

    change position require_once

--- project/public/index.php
+++ project/public/index.php
@@ -1,7 +1,7 @@
 <?php 
-require_once "../App/config/routes.php";
 require "../vendor/autoload.php";
 use App\Core\Router;
 
 $router = new Router();
+require_once "../App/config/routes.php";
 $router->dispatch($_SERVER['REQUEST_URI'],$_SERVER['REQUEST_METHOD']);
